a4c562176819ef8c7e5087bd833531b32c014c6d
Misiu Pajor <mpajor@op5.com>
2017-09-15 13:15:36 +0200

    Added support for triggering hooks: prestart, poststart, poststop

--- entrypoint.d/entrypoint.sh
+++ entrypoint.d/entrypoint.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 #set -eu pipefail
+set -x
+
+function trigger_hooks() {
+    echo "Triggering ${1} hooks"
+    /usr/libexec/entrypoint.d/hooks.py $1
+}
+
+# trigger prestart hooks defined in hooks.json
+trigger_hooks prestart
 
 # import backup file
 if [ ! -z "$import_backup" ]; then
@@ -12,17 +21,6 @@ if [ ! -z "$import_backup" ]; then
     fi
 fi
 
-function trigger_hooks() {
-	/usr/libexec/entrypoint.d/hooks.py $1
-}
-
-# execute start hooks
-trigger_hooks start
-
-# execute shutdown hooks when gracefully shutdown
-trap 'trigger_hooks stop' SIGTERM
-trap 'trigger_hooks stop' SIGINT
-
 # start OP5 related services
 services=("sshd" "mysqld" "merlind" "naemon" "httpd" "nrpe" "processor" "collector" "rrdcached" "synergy" "smsd" "postgresql")
 for i in "${services[@]}"
@@ -30,5 +28,16 @@ do
     service $i start
 done
 
+
 exec "$@"
-sleep infinity
+# trigger poststop hooks defined in hooks.json
+trap 'kill ${!}; trigger_hooks poststop' SIGTERM
+
+# trigger poststart defined in hooks.json
+trigger_hooks poststart
+
+# wait indefinitely
+    while true
+    do
+      tail -f /dev/null & wait ${!}
+    done
